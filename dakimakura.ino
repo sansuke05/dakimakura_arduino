c440a990fa51ba6936406041947c11a8d2f021eb
Takanori Watanabe <takanori5064@gmail.com>
2017-07-25 13:03:51 +0900

    first commit

---
+++ dakimakura.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#define buttonPin A0
+
+int buttonState = 0;
+SoftwareSerial android(12,13);
+
+void setup() {
+  android.begin(115200);
+  Serial.begin(19200);
+
+  Serial.write("init");
+
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if(buttonState == HIGH || Serial.available()) {
+    Serial.write("Pushed!\n");
+    android.write("1");
+    delay(1000);
+  }
+
+}
